cfd239997f316c60081755dee176a93c30858250
Victor Aprea <victor.aprea@wickeddevice.com>
2020-10-31 17:55:18 -0400

    drop rows that don't have dates

--- run.js
+++ run.js
@@ -1,3 +1,4 @@
+// jshint esversion: 8
 const fs = require('fs');
 const argv = require('minimist')(process.argv.slice(2));
 const parse = require('csv-parse/lib/sync');
@@ -5,7 +6,7 @@ const stringify = require('csv-stringify/lib/sync');
 const path = require('path');
 const moment = require('moment');
 
-const inputFilename = path.resolve('./', argv.i || argv.input || 'sensitire.csv');
+const inputFilename = path.resolve('./', argv.i || argv.input || 'sensititre.txt');
 const outputFilename = path.resolve('./', argv.o || argv.output || 'output.csv');
 
 function columnNumberToLetter(num) {
@@ -21,21 +22,27 @@ async function run() {
 
         let plateOffset = 0;
         let foundDate = false;
-        for (const value of inputParsed[0]) {
-            if (moment(value, 'M/DD/YYYY H:mm', true).isValid()) {
-                console.log('Found valid date string at column ' + columnNumberToLetter(plateOffset));
-                foundDate = true;
-                break;
-            }
-            plateOffset++;
+        for (const row of inputParsed) {
+          if (foundDate) {
+            break;
+          }
+          plateOffset = 0;
+          for (const value of row) {
+              if (moment(value, 'M/DD/YYYY H:mm', true).isValid()) {
+                  console.log('Found valid date string at column ' + columnNumberToLetter(plateOffset));
+                  foundDate = true;
+                  break;
+              }
+              plateOffset++;
+          }
         }
-        
+
         if (!foundDate) {
             throw new Error('Did not find a valid date in first row')
         }
         // the next column is the plate offset
         plateOffset++;
-        console.log('Plate data starts at column ' + columnNumberToLetter(plateOffset));
+        console.log('Drug data starts at column ' + columnNumberToLetter(plateOffset));
 
         const uniquePlates = new Set();
         for (const row of inputParsed) {
@@ -47,11 +54,19 @@ async function run() {
         }
 
         const plateNames = Array.from(uniquePlates).sort();
-        console.log('Unique plate names: ', JSON.stringify(plateNames));
+        console.log('Unique drugs: ', JSON.stringify(plateNames));
         const outputParsed = [];
-        
+
         console.log('Reorganizing columns');
+        let numRowsDroppedForDateViolation = 0;
+
         for (const row of inputParsed) {
+            // a valid date is required right before the plateOffset
+            const date = row[plateOffset - 1];
+            if (!moment(date, 'M/DD/YYYY H:mm', true).isValid()) {
+              numRowsDroppedForDateViolation++;
+              continue;
+            }
             let outputRow = row.slice(0, plateOffset); // copy up to the plate data
             for (const plate of plateNames) {
                 const idx = row.findIndex(v => v === plate);
@@ -63,11 +78,12 @@ async function run() {
             }
             outputParsed.push(outputRow);
         }
-        
-        console.log('Done rerganizing columsn');
+
+        console.log('Done rerganizing columns');
+        console.log(`Dropped ${numRowsDroppedForDateViolation} rows because of missing date`);
         outputString = stringify(outputParsed, {header: false, quoted_string: true});
         fs.writeFileSync(outputFilename, outputString, 'utf8');
-        console.log(`Done writing file "${outputFilename}"`)
+        console.log(`Done writing file "${outputFilename}"`);
     } catch (e) {
         console.log('Error: ' + e.message, e.stack);
     }
