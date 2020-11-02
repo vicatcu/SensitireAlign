df5c7eb2d418827bbba306ddd3a5a40a8c44f971
Victor Aprea <victor.aprea@wickeddevice.com>
2020-11-01 23:17:01 -0500

    expose date format as parameter

--- run.js
+++ run.js
@@ -6,7 +6,7 @@ const stringify = require('csv-stringify/lib/sync');
 const path = require('path');
 const moment = require('moment');
 const inputEncoding = argv.e || argv.encoding || 'utf16le';
-const dateFormat = 'YYYY-MM-DD HH:mm:ss';
+const dateFormat = argv.d || argv.date || 'YYYY-MM-DD HH:mm:ss';
 
 const inputFilename = path.resolve('./', argv.i || argv.input || 'sensititre.txt');
 const outputFilename = path.resolve('./', argv.o || argv.output || 'output.csv');
